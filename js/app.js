16c617dd5f9f58f91feb1039dbd041f8b86ef6c0
kris riedman <riedmank@gmail.com>
2018-08-01 13:33:55 -0700

    update html and added js for last 2 questions

--- js/app.js
+++ js/app.js
@@ -1,125 +1,77 @@
 'use strict';
 
-// var answer1;
-// var answer2;
-// var answer3;
-// var answer4;
-// var answer5;
-
 var myElement = document.getElementById('myButton');
-var check = false;
-var i;
-var answer = [5];
-var userAnswer;
-
-function checker() {
-  while (check === false) {
-    userAnswer = prompt('Answer for question ' + (i + 1) + '?').toLowerCase();
-    if (userAnswer === 'yes' || userAnswer === 'y' || userAnswer === 'no' || userAnswer === 'n') {
-      answer.push(userAnswer);
-      console.log('User answer for question ' + (i + 1) + ': ' + userAnswer);
-    } else {
-      alert('Please enter one of the following: yes, y, no, n.');
-      continue;
-    }
-    check = true;
-  }
-}
-
-function solution() {
-  alert('Solutions:\nQuestion 1: yes \nQuestion 2: no\nQuestion 3: no\nQuestion 4: yes\nQuestion 5: no');
-}
+// var myElement1 = document.getElementById('facts');
 
 myElement.addEventListener('click', function() {
-  //js to change first article needed!!
-  for (i = 0; i < 5; i++) {
-    checker();
-    check = false;
+  // myElement1.style.background-color='darkgreen';
+  
+  //instance variables
+  var check = false;
+  var answer = [7];
+  var userAnswer;
+  var guess;
+  var secretNumber = Math.floor(Math.random() * 10) + 1;
+  var places = ['washington', 'north Dakota', 'california', 'florida'];
+  
+  // //about me quiz
+  // for (var i = 0; i < 5; i++) {
+  //   while (check === false) {
+  //     userAnswer = prompt('Answer for question ' + (i + 1) + '?').toLowerCase();
+  //     if (userAnswer === 'yes' || userAnswer === 'y' || userAnswer === 'no' || userAnswer === 'n') {
+  //       answer.push(userAnswer);
+  //       console.log('User answer for question ' + (i + 1) + ': ' + userAnswer);
+  //       check = true;
+  //     } else {
+  //       alert('Please enter one of the following: yes, y, no, n.');
+  //     }
+  //   }
+  //   check = false;
+  // }
+
+  // //guessing game
+  // alert('Try to guess my favorite number between 1 and 10 in 4 guesses or less!');
+  // for (var j = 4; j > 0; j--) {
+  //   console.log(secretNumber);
+  //   guess = prompt('What is your guess? ' + j + ' guesses remaining.');
+  //   guess = parseInt(guess); 
+  //   if (isNaN(guess) || guess < 1 || guess > 10) {
+  //     alert('You didn\'t enter a correct value. Try again.');
+  //     j++;
+  //   } else if (guess < secretNumber ){
+  //     alert('Your guess was too low.');
+  //   } else if (guess > secretNumber ) {
+  //     alert('Your guess was too high.');
+  //   } else if (guess === secretNumber) {
+  //     alert('You are correct! You had ' + j + ' guesses remaining.');
+  //     break;
+  //   } else {
+  //     alert('Try again.');
+  //   }
+  // }
+  // if (j === 0) {
+  //   alert('You failed to guess my favorite number. It was ' + secretNumber);
+  // }
+
+  //State guessing game
+  check = false;
+  alert('Try to guess a State I have visited.');
+  for (var k = 6; k > 0; k--) {
+    userAnswer = prompt('What is your guess? You have ' + k + ' guesses remaining.').toLowerCase();
+    for (var l = 0; l < places.length; l++) {
+      if (userAnswer === places[l])
+      {
+        check = true;
+      }
+    }
+    if (check) {
+      alert('You have correctly guessed a State where I have visited!');
+      break;
+    } else {
+      alert('You were incorrect. Try again.');
+    }
   }
-  solution();
-  // while (check === false) {
-  //   answer1 = prompt('Was Kris born in North Dakota?').toLowerCase();
-  //   if (answer1 === 'yes' || answer1 === 'y' || answer1 === 'no' || answer1 === 'n') {
-  //     console.log('User answer for question 1: ' + answer1);
-  //     if (answer1 === 'yes' || answer1 === 'y')
-  //     {
-  //       alert('You are correct');
-  //     } else {
-  //       alert('Wrong');
-  //     }
-  //   } else {
-  //     alert('Please enter one of the following: yes, y, no, n.');
-  //     continue;
-  //   }
-  //   check = true;
-  // }
-  // check = false;
-  // while (check === false) {
-  //   answer2 = prompt('Kris\' hometown is Yakima, ND?').toLowerCase();
-  //   if (answer2 === 'yes' || answer2 === 'y' || answer2 === 'no' || answer2 === 'n') {
-  //     console.log('User answer for question 2: ' + answer2);
-  //     if (answer2 === 'no' || answer2 === 'n')
-  //     {
-  //       alert('You are correct');
-  //     } else {
-  //       alert('Wrong');
-  //     }
-  //   } else {
-  //     alert('Please enter one of the following: yes, y, no, n.');
-  //     continue;
-  //   }
-  //   check = true;
-  // }
-  // check = false;
-  // while (check === false) {
-  //   answer3 = prompt('Kris played the trombone in highschool?').toLowerCase();
-  //   if (answer3 === 'yes' || answer3 === 'y' || answer3 === 'no' || answer3 === 'n') {
-  //     console.log('User answer for question 3: ' + answer3);
-  //     if (answer3 === 'no' || answer3 === 'n')
-  //     {
-  //       alert('You are correct');
-  //     } else {
-  //       alert('Wrong');
-  //     }
-  //   } else {
-  //     alert('Please enter one of the following: yes, y, no, n.');
-  //     continue;
-  //   }
-  //   check = true;
-  // }
-  // check = false;
-  // while(check === false) {
-  //   answer4 = prompt('Was Kris in the Air Force stationed in Minot, ND?').toLowerCase();
-  //   if (answer4 === 'yes' || answer4 === 'y' || answer4 === 'no' || answer4 === 'n') {
-  //     console.log('User answer for question 4: ' + answer4);
-  //     if (answer4 === 'yes' || answer4 === 'y')
-  //     {
-  //       alert('You are correct');
-  //     } else {
-  //       alert('Wrong');
-  //     }
-  //   } else {
-  //     alert('Please enter one of the following: yes, y, no, n.');
-  //     continue;
-  //   }
-  //   check = true;
-  // }
-  // check = false;
-  // while(check === false) {
-  //   answer5 = prompt('Does Kris have a pet dog named Max?').toLowerCase();
-  //   if (answer5 === 'yes' || answer5 === 'y' || answer5 === 'no' || answer5 === 'n') {
-  //     console.log('User answer for question 5: ' + answer5);
-  //     if (answer5 === 'no' || answer5 === 'n')
-  //     {
-  //       alert('You are correct');
-  //     } else {
-  //       alert('Wrong');
-  //     }
-  //   } else {
-  //     alert('Please enter one of the following: yes, y, no, n.');
-  //     continue;
-  //   }
-  //   check = true;
-  // }
-  // check = false;
+  if (k === 0) {
+    alert('You failed to guess a State where I visited.');
+  }
 });
